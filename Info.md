eef2a0c2747369037ee83c898fa10b927cf0c98e
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-06-19 10:20:33 +0530

    info

---
+++ Info.md
@@ -0,0 +1,17 @@
+    # function
+    # Set of code that perfoms a sepecific task.
+    # it allows you to modularize your code and reuse it in different parts of your program.
+    # write clean code and make it easier to read and maintain.
+    #It has tWo types of function:
+    # 1. Built-in functions
+        #The function which are coming with python are called built-in functions.
+        #Print(), len(), type(), input(), range() are some of the built-in functions in python.
+    #2. User-defined functions
+        #The function which are defined by the user are called user-defined functions.
+        #explicitly defined by the user to perform a specific task.
+    
+    #3.How to create a function in python ?
+    # Syntax: def function_name(parameters):
+    #           # code block
+    #           # return value
+    #   
--- dictionary.py
+++ dictionary.py
@@ -1,26 +1,83 @@
-car={
-    "model":"Toyota",
-    "year":2020,
-    "color":"Red"
-}
+# car={
+#     "model":"Toyota",
+#     "year":2020,
+#     "color":"Red"
+# }
 
-x=car.get("model")
+# x=car.get("model")
 
-print(x)
+# print(x)
 
-# // Output: Toyota
-# // Set Example
+# # // Output: Toyota
+# # // Set Example
 
-my_set={"anshu", "sacchin", "Kholi"}
-my_set2={"anshu", "sacchin", "Kholi"}
+# my_set={"anshu", "sacchin", "Kholi"}
+# my_set2={"anshu", "sacchin", "Kholi"}
 
-my_set.add("Rohit")
-my_set.remove("anshu")
+# my_set.add("Rohit")
+# my_set.remove("anshu")
 
-result=my_set.union(my_set2)
+# result=my_set.union(my_set2)
 # y="Anshu" in my_set
 
 
-print(my_set)
-# print(y)
-print(result)
+# print(my_set)
+# # print(y)
+# print(result)
+
+
+# LOOPS
+# i=100
+# while i>=1:
+#         print(i)
+#         i-=1
+
+
+# Table 
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print(i*j)
+#         print()
+
+
+# Odd no
+
+# for i in range (1,20):
+#      if i % 2 == 1:
+#         print(i)
+
+
+# find the index of a number in a tuppel 
+# def tuppel():
+#     tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#     num = int(input("Enter a Number: "))
+#     n = 0
+#     t=num
+#     for i in tup:
+#         if i == num:
+#             print("Number found at index", n)
+#             return
+#         n = n + 1
+#     print("Number is not Found")
+
+# tuppel()
+
+
+# if else 
+def tuppel ():
+    tup=(1,2,3,4,5)
+    num=tup
+    user=int (input("Enter a number:"))
+    n=0
+    for n, item in enumerate(tup):
+        if item == user:
+            print("Index is ",n)
+            return 
+    print("Number not Found ")
+
+    tuppel()
+
+
+
+
