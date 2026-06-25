bbe3786ab34f125cefd5d85245095dbb55ebe624
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-06-25 11:20:11 +0530

    fibonachi

---
+++ faabionachi.py
@@ -0,0 +1,14 @@
+# fibonacci series using recursion
+x=int(input("Enter a number:"))
+
+def fibonacci(n):
+    if n<=0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print("Fibonacci series:" ,end="")
+for i in range(x):
+    print(fibonacci(i),end=" ")
